80d026a9f60f80a8379d3200546eacd856a64475
r-lyeh <rlyeh.2015+github@gmail.com>
2018-08-03 14:20:04 +0200

    Fix linkage

--- engine/detect/detect_linkage.c
+++ engine/detect/detect_linkage.c
@@ -3,10 +3,25 @@
 
 // symbol linkage and visibility
 
+#ifdef _MSC_VER
+#define IMPORT __declspec(dllimport)
+#define EXPORT __declspec(dllexport)
+#define STATIC
+#else
+#define IMPORT extern
+#define EXPORT __attribute__((visibility("default")))
+#define STATIC
+#endif
+
+#ifndef LINKAGE
+#define LINKAGE STATIC
+#endif
+
+/*
 #if   !defined(IMPORT) && defined(_MSC_VER)
 #define IMPORT __declspec(dllimport)
 #elif !defined(IMPORT)
-#define IMPORT __attribute__((visibility("default")))
+#define IMPORT extern
 #endif
 
 #if   !defined(EXPORT) && defined(_MSC_VER)
@@ -18,9 +33,6 @@
 #ifndef STATIC
 #define STATIC
 #endif
-
-#ifndef LINKAGE
-#define LINKAGE STATIC
-#endif
+*/
 
 #endif
