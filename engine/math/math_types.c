82f04e4919ff82b84fb65092b596b8cd5ee2d788
r-lyeh <rlyeh.2015@gmail.com>
2019-04-11 23:30:51 +0200

    engine: add uint types

--- engine/math/math_types.c
+++ engine/math/math_types.c
@@ -5,20 +5,24 @@
 #include <stdint.h>
 #include "math_linear.c" // M_CAST
 
-typedef struct byte2 { uint8_t x,y;     } byte2;
-typedef struct byte3 { uint8_t x,y,z;   } byte3;
+typedef struct byte2 { uint8_t x,y; } byte2;
+typedef struct byte3 { uint8_t x,y,z; } byte3;
 typedef struct byte4 { uint8_t x,y,z,w; } byte4;
 
-typedef struct int2 { int x,y;     } int2;
-typedef struct int3 { int x,y,z;   } int3;
+typedef struct int2 { int x,y; } int2;
+typedef struct int3 { int x,y,z; } int3;
 typedef struct int4 { int x,y,z,w; } int4;
 
-typedef struct float2 { float x,y;     } float2;
-typedef struct float3 { float x,y,z;   } float3;
+typedef struct uint2 { unsigned int x,y; } uint2;
+typedef struct uint3 { unsigned int x,y,z; } uint3;
+typedef struct uint4 { unsigned int x,y,z,w; } uint4;
+
+typedef struct float2 { float x,y; } float2;
+typedef struct float3 { float x,y,z; } float3;
 typedef struct float4 { float x,y,z,w; } float4;
 
-typedef struct double2 { double x,y;     } double2;
-typedef struct double3 { double x,y,z;   } double3;
+typedef struct double2 { double x,y; } double2;
+typedef struct double3 { double x,y,z; } double3;
 typedef struct double4 { double x,y,z,w; } double4;
 
 #define byte2(x,y)       M_CAST(byte2, (uint8_t)(x), (uint8_t)(y) )
@@ -28,6 +32,10 @@ typedef struct double4 { double x,y,z,w; } double4;
 #define int2(x,y)        M_CAST(int2, (int)(x), (int)(y) )
 #define int3(x,y,z)      M_CAST(int3, (int)(x), (int)(y), (int)(z) )
 #define int4(x,y,z,w)    M_CAST(int4, (int)(x), (int)(y), (int)(z), (int)(w) )
+
+#define uint2(x,y)       M_CAST(uint2, (unsigned)(x), (unsigned)(y) )
+#define uint3(x,y,z)     M_CAST(uint3, (unsigned)(x), (unsigned)(y), (unsigned)(z) )
+#define uint4(x,y,z,w)   M_CAST(uint4, (unsigned)(x), (unsigned)(y), (unsigned)(z), (unsigned)(w) )
 
 #define float2(x,y)      M_CAST(float2, (float)(x), (float)(y) )
 #define float3(x,y,z)    M_CAST(float3, (float)(x), (float)(y), (float)(z) )
